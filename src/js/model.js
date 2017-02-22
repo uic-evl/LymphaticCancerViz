675e0266247bc378bdf132ed9b37055228b5d968
onorinbejasus <tluciani21@gmail.com>
2017-02-22 11:20:45 -0600

    -- Implemented different coloring for left and right nodes

--- src/js/model.js
+++ src/js/model.js
@@ -52,8 +52,8 @@ function Patients() {
           "patient"  : patient.id,
           "nodes"    :
               _.chain(patient.nodes)
-                  // .filter(function(p){ return p[0] === 'L';})
-                  .map(function(p){ return p.substring(1)}).value(),
+                  .partition(function(p){ return p[0] === 'L';}).value(),
+                  //.partition(function(p){ return p.substring(1)}).value(),
           "position" : patient.position,
           "gender"   : patient.gender
         };
