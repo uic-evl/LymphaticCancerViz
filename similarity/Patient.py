0d97f088c9e62946a0df6fc9c6b239e5ffb75b4f
onorinbejasus <tluciani21@gmail.com>
2017-02-27 12:14:03 -0600

    -- Added the option to sort by Jaccard or Tanimoto -- Recomputed the data

--- similarity/Patient.py
+++ similarity/Patient.py
@@ -1,37 +1,32 @@
 import numpy as np
 from Graph import Graph
 
-class Patient(object):
 
-    def __init__(self, id):
-        self.id = id
+class Patient(object):
+    def __init__(self, p_id):
+        self.id = p_id
 
         self.left_graph = None
         self.right_graph = None
         self.tumor_position = ""
         self.gender = ""
-
+        self.lymph_nodes = []
 
     def set_lymph_nodes(self, lymph_nodes):
         self.lymph_nodes = lymph_nodes
 
-
-    def set_graphs(self, leftGraph, rightGraph):
-        self.left_graph = leftGraph
-        self.right_graph = rightGraph
-
+    def set_graphs(self, left_graph, right_graph):
+        self.left_graph = left_graph
+        self.right_graph = right_graph
 
     def set_gender(self, gender):
         self.gender = gender
 
-
-    def set_tumor_position(self, tumorPosition):
-        self.tumor_position = tumorPosition
-
+    def set_tumor_position(self, tumor_position):
+        self.tumor_position = tumor_position
 
     def get_id(self):
         return self.id
-
 
     def get_graph(self, position):
         if position == "Right":
@@ -39,18 +34,17 @@ class Patient(object):
         elif position == "Left":
             return self.left_graph
 
-
     def get_all_nodes(self):
         return self.left_graph.get_nodes() + self.right_graph.get_nodes()
 
+    def get_all_unique_nodes(self):
+        return list(set(self.left_graph.get_nodes() + self.right_graph.get_nodes()))
 
     def get_tumor_position(self):
         return self.tumor_position
 
-
     def get_gender(self):
         return self.gender
-
 
     def get_vector(self):
         left_nodes = self.left_graph.get_nodes()
--- similarity/Similarity.py
+++ similarity/Similarity.py
@@ -7,12 +7,16 @@ def compute_jaccard_coeff(items_a, items_b):
     intersection_length = len(intersection)
 
     # return the Jaccard coefficient: ( |A /\ B| / (|A| + |B| -|A /\ B|) )
-    return float(intersection_length) / float((len(items_a) + len(items_b) - intersection_length)) + 1
+    return float(intersection_length) / float((len(items_a) + len(items_b) - intersection_length))
 
 def compute_tanimoto_coeff(vector_a, vector_b):
     a_dot_b = np.dot(vector_a, vector_b)
     len_a = np.sum(vector_a)
     len_b = np.sum(vector_b)
+
+    # denominator is zero, no similarity
+    if (len_a * len_a + len_b * len_b - a_dot_b) == 0:
+        return 0
 
     # return the Tanimoto coefficient: ( (A dot B) / (|A|^2 + |B|^2 - (A dot B)) )
     tanimoto_coeff = a_dot_b / (len_a * len_a + len_b * len_b - a_dot_b)
