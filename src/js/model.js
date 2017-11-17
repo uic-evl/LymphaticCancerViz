39bb1d36dd20c620654749ab3b8a1a097d5f3c8c
onorinbejasus <tluciani21@gmail.com>
2017-11-16 18:43:02 -0600

    -- Added David's new predictions along with a new menu to cluster them

--- src/js/model.js
+++ src/js/model.js
@@ -15,6 +15,7 @@ function Patients() {
     self.rankings = ko.observableArray();
     self.optionsCaption = ko.observable('Select a Patient');
     self.clusterCaption = ko.observable('Select a Cluster');
+    self.predictionCaption = ko.observable('Select a Variable');
 
     self.patients = ko.observableArray();
     App.data.forEach(function (patient) {
@@ -34,8 +35,7 @@ function Patients() {
       self.cluster_groups = _.partition(cluster_groups, function(o) {
         return (o.name.split('_')[0] === "weighted")
       } );
-      self.sortingAlgorithms = ko.observableArray(["Tanimoto Weighted", "Tanimoto Nodes",
-        // "Tanimoto Edges",  "Jaccard"
+      self.sortingAlgorithms = ko.observableArray(["Tanimoto Weighted" /*, "Tanimoto Nodes", "Tanimoto Edges",  "Jaccard" */
       ]);
     }
     else {
@@ -43,21 +43,41 @@ function Patients() {
       self.sortingAlgorithms = ko.observableArray(["Tanimoto Weighted"]);
     }
 
+    let prediction_groups = [];
+    for(let i = 0; i < 20; i++){
+      prediction_groups.push(parseFloat(i)/20);
+    }
+
+    self.prediction_groups = [];
+    prediction_groups.forEach(function(p,i){
+      if(i === 0) return;
+      let group = "" + (prediction_groups[i-1]) + " <= p < " + p;
+      self.prediction_groups.push(group);
+    });
+    self.predictionVariable = ko.observableArray(["Feeding Tube","Aspirating","Enjoyment"]);
+
     // clusters
     self.clusters = ko.observableArray();
+    self.predictions = ko.observableArray();
 
-    self.selections = ko.observableArray(["By Patient", "By Cluster"]);
+    self.selections = ko.observableArray(["By Prediction", "By Patient"/*, "By Cluster"*/]);
     self.numberToDisplay = ko.observableArray([50, 100, 'All']);
+
     self.currentPatient = ko.observable(undefined);
     self.currentSorting = ko.observable(self.sortingAlgorithms[0]);
     self.currentDisplay = ko.observable(self.numberToDisplay[0]);
     self.currentCluster = ko.observable();
+    self.currentPrediction = ko.observable();
+    self.currentPredictionVariable = ko.observable();
     self.currentType = ko.observable(self.selections[0]);
   }
 
   function filterPatients(patient) {
     // filter the patients based on similarity
     patient.similarity.forEach(function (id, i) {
+      let p  = _.find(self.patients(), {id: id});
+      if(!p) return;
+
       let site = _.find(App.sites, {patient: id});
 
       /* I just want the first 50, etc */
@@ -103,7 +123,7 @@ function Patients() {
     self.optionsCaption(undefined);
 
     if(self.currentType() === "By Patient"){
-      dropdown.firstChild.textContent = "ChiSquared Cluster";
+      //dropdown.firstChild.textContent = "ChiSquared Cluster";
       self.currentCluster(undefined);
 
       let site = _.find(App.sites, {patient: newValue.id}),
@@ -123,7 +143,7 @@ function Patients() {
     filterPatients(newValue);
     placeSelectedPatientFirst(newValue);
 
-    /// / Render to the screen
+    // Render to the screen
     App.createVisualizations(self.rankings());
   }
 
@@ -156,7 +176,7 @@ function Patients() {
 
     /* Reset the cluster drop down*/
     self.currentCluster(undefined);
-    dropdown.firstChild.textContent = "ChiSquared Cluster";
+    // dropdown.firstChild.textContent = "ChiSquared Cluster";
 
     /* Touch the current observable to re-render the scene */
     if(self.currentPatient()) {
@@ -179,6 +199,69 @@ function Patients() {
     }
   }
 
+  function changeProbabilityRange(newValue){
+    /* Clear the default caption if it exists */
+    self.optionsCaption(undefined);
+    // clear the patient list
+    self.rankings.removeAll();
+    self.patients.removeAll();
+
+    let split = newValue.split(" <= p < "),
+        min = parseFloat(split[0]), max = parseFloat(split[1]),
+        idx = self.prediction_groups.indexOf(newValue),
+        variable = _.chain(self.currentPredictionVariable()).toLower().replace(" ", "_").value();
+
+    // add to the list only the patients in the current cluster
+    _.filter(App.sites, function(s) {
+      return s.predictions[variable] >= min && s.predictions[variable] < max;
+    })
+      .forEach(function(p){
+        // find the patient in the data and add it to the list
+        let patient = _.find(App.data, function (o) {
+          return o.id === p.patient;
+        });
+        self.patients.push(_.clone(patient));
+      });self.currentPrediction
+
+    self.currentPatient(self.patients()[0]);
+
+    // App.sites.forEach(function(s){
+    //   if(s.predictions[variable] >= min && s.predictions[variable] < max){
+    //     //console.log(s.predictions[variable]);
+    //     self.rankings.push(s);
+    //     console.log(s.predictions[variable]);
+    //   }
+    // });
+
+    // Render to the screen
+    //App.createVisualizations(self.rankings());
+    //console.log(self.rankings());
+
+  }
+
+  function changeProbabilityVariable(newValue){
+    /* Clear the default caption if it exists */
+    self.predictionCaption(undefined);
+    self.currentPredictionVariable(newValue);
+    self.currentPrediction(undefined);
+    self.predictions.removeAll();
+
+    let variable = _.chain(newValue).toLower().replace(" ", "_").value();
+    self.prediction_groups.forEach(function(p){
+      let split = p.split(" <= p < "),
+        min = parseFloat(split[0]), max = parseFloat(split[1]);
+
+      let r = _.find(App.sites, function(s){
+        return s.predictions[variable] >= min && s.predictions[variable] < max;
+      });
+      /* If any of the elements falls into this bin */
+      if(r){
+        self.predictions.push(p);
+      }
+    });
+
+  }
+
   function changeFilteringMode(newValue) {
 
     if(newValue === "By Patient"){
@@ -188,7 +271,10 @@ function Patients() {
       self.patients.removeAll();
       self.rankings.removeAll();
 
+      self.currentPredictionVariable(undefined);
+      self.currentPrediction(undefined);
       self.currentPatient(undefined);
+
       self.currentSorting(self.sortingAlgorithms[0]);
 
       App.data.forEach(function (patient) {
@@ -201,6 +287,10 @@ function Patients() {
 
       self.currentCluster(undefined);
       self.currentPatient(undefined);
+
+      self.currentPrediction(undefined);
+      self.currentPredictionVariable(undefined);
+
       self.rankings.removeAll();
       self.clusters.removeAll();
 
@@ -208,9 +298,19 @@ function Patients() {
         self.clusters.push(c);
       });
     }
+    else if(newValue === "By Prediction"){
+      self.optionsCaption = ko.observable('Select a Probability');
 
-    dropdown.firstChild.textContent = "ChiSquared Cluster";
-    self.currentCluster(undefined)
+      self.currentCluster(undefined);
+      self.currentPatient(undefined);
+      self.currentPredictionVariable(undefined);
+      self.currentPrediction(undefined);
+
+      self.rankings.removeAll();
+      self.clusters.removeAll();
+
+    }
+
   }
 
   function setupMenu() {
@@ -255,13 +355,13 @@ function Patients() {
           });
           self.currentPatient(self.patients()[0]);
       }
-        /* Touch the current observable to re-render the scene */
-        else if(self.currentPatient()) {
-          self.rankings.removeAll();
-          filterPatients(self.currentPatient());
-          // Render to the screen
-          App.createVisualizations(self.rankings());
-        }
+      /* Touch the current observable to re-render the scene */
+      else if(self.currentPatient()) {
+        self.rankings.removeAll();
+        filterPatients(self.currentPatient());
+        // Render to the screen
+        App.createVisualizations(self.rankings());
+      }
       }
     });
   }
@@ -269,13 +369,16 @@ function Patients() {
   // initialize the observables
   setupObservables();
   // initialize the menu
-  setupMenu();
+  //setupMenu();
 
   // subscribe to the change of the selection
   self.currentPatient.subscribe(changeCurrentPatient);
 
   /*Subscribe the the change in similarity metric */
   self.currentSorting.subscribe(changeCurrentSorting);
+
+  self.currentPrediction.subscribe(changeProbabilityRange);
+  self.currentPredictionVariable.subscribe(changeProbabilityVariable);
 
   // subscribe to the change of the how many patients to display
   self.currentDisplay.subscribe(changeNumberOfPatients);
@@ -288,17 +391,27 @@ function Patients() {
 /*** IFE to load the data and apply the KO bindings ***/
 (function () {
 
+  function round(value, step) {
+    step || (step = 1.0);
+    let inv = 1.0 / step;
+    return Math.ceil(value * inv) / inv;
+  }
+
+  function bin_prediction(p) {
+    return round(p, 0.05);
+  }
+
   queue()
     .defer(d3.json, "data/json/tanimoto_nodes.json")
     .defer(d3.json, "data/json/tanimoto_weighted.json")
     .defer(d3.csv, "data/csv/cluster_results_weighted_complete_0818.csv")
+    .defer(d3.csv, "data/csv/predict_outcome_lymph.csv")
     // .defer(d3.csv, "data/csv/cluster_results_weighted_complete_0811.csv")
     // .defer(d3.csv, "data/csv/cluster_results_weighted_0810.csv")
     // .defer(d3.csv, "data/csv/cluster_results_simple_0803.csv")
     // .defer(d3.json, "data/json/tanimoto_edges.json")
     // .defer(d3.json, "data/json/jaccard.json")
-      .await(function (error,  nodes, weighted, clusters//, edges, jaccard
-      ) {
+      .await(function (error,  nodes, weighted, clusters, predictions /*, edges, jaccard*/) {
       if (error) return console.warn(error);
 
       // App.edges = edges;
@@ -320,13 +433,15 @@ function Patients() {
       });
 
         /* Iterate through the data and pull out each patient's information */
-      App.nodes.forEach(function (patient) {
-
-        // if(patient.nodes.length <= 1)
-        //   return;
+      App.weighted.forEach(function (patient) {
 
         // extract the clusters based on the patient's id
         let patient_clusters = _.find(clusters, function(o){ return parseInt(o.pid) === patient.id });
+        /* extract the prediction clusters based on the id */
+        let pred = _.chain(predictions)
+          .find(function(o){ return parseInt(o.id) === patient.id })
+          .omit(["", "id"]).value();
+        /* Extract the infected lymph nodes of the patient */
         let nodes = _.chain(patient.nodes).partition(function (p) {
           return p[0] === 'L';
         }).value();
@@ -349,14 +464,34 @@ function Patients() {
           "feedingTube_pre": !!patient["Tube_removal"] ? "N": patient["Feeding_tube_6m"],
           "aspiration_pre" : patient["Aspiration_rate_Pre-therapy"] ? patient["Aspiration_rate_Pre-therapy"] : "NA" ,
           "aspiration_post" : patient["Aspiration_rate_Post-therapy"] ? patient["Aspiration_rate_Post-therapy"] : "NA",
-          clusters: _.omit(patient_clusters, ["pid", ""])
+          "clusters": _.omit(patient_clusters, ["pid", ""]),
+          "predictions":
+            {
+              "enjoyment": parseFloat(pred.pred_Enjoy),
+              "enjoyment_bin": bin_prediction(parseFloat(pred.pred_Enjoy)),
+              "feeding_tube": parseFloat(pred.prob_feeding_tube),
+              "feeding_tube_bin": bin_prediction(parseFloat(pred.prob_feeding_tube)),
+              "aspirating": parseFloat(pred.prob_aspiration),
+              "aspirating_bin": bin_prediction(parseFloat(pred.prob_aspiration)),
+            }
         };
         App.sites.push(site);
       });
 
       App.data = App.weighted;
 
-      ko.applyBindings(new Patients());
+      /* Load the dropdown templates */
+      let d1 =  new $.Deferred(),
+          d2 =  new $.Deferred(),
+          d3 =  new $.Deferred();
+
+      $.when(d1,d2,d3).done(function(){
+        ko.applyBindings(new Patients());
+      });
+
+      $("#byPatient").load("src/htmlTemplates/byPatient.html", ()=>{d1.resolve()});
+      $("#byCluster").load("src/htmlTemplates/byCluster.html", ()=>{d2.resolve()});
+      $("#byPrediction").load("src/htmlTemplates/byPrediction.html", ()=>{d3.resolve()});
     });
 
 })();
