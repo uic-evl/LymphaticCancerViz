78386257b1d645a18eb9427965faf6a7c9db8688
Timothy Luciani <onorinbejasus@gmail.com>
2018-05-16 11:17:06 -0500

    -- Added the clustering files to track them

---
+++ HAC/run_HAC_All.m
@@ -0,0 +1,20 @@
+filename = '5_2018\weighted_matrix.csv';
+dataFile = '5_2018\Tanimoto_weighted_Data_and_Scores_5_2018.csv';
+outFile = '5_2018\Tanimoto_weighted_Data_and_Scores_5_2018';
+
+% linkage = {'complete','weighted'};
+linkage = {'complete'};
+
+% cuts = {{7.4,6,5.85,5.85,5.85,5.85}, {7.4,6,5.85,5.85,5.85,5.85}};
+cuts = {{6}};%,{7.8}};
+% groups = {{3,4,5,6,7,8},{3,4,5,6,7,8}};
+groups = {{6}};%,{6}};
+
+for i=1:length(linkage) 
+    link = linkage{i};
+    for j=1:length(groups{i})
+        k=groups{i}{j};
+        cut = cuts{i}{j}; 
+        clusterLymphSim (link, k, cut, filename, dataFile, outFile);
+    end
+end
