5116e3c043a76e624fcd090b2d05fd8ed159ca7e
Timothy Luciani <onorinbejasus@gmail.com>
2018-06-12 21:37:22 -0500

    -- Working with the clusters

--- src/js/model.js
+++ src/js/model.js
@@ -722,7 +722,7 @@ function Patients() {
         return groups;
     }
 
-       App.utils.readFiles(function (error, weighted, weighted_w6, bigrams_w6, nodes_w6
+       App.utils.readFiles(function (error, weighted, weighted_w6,weighted_w30, nodes_w6, nodes_w30
                                          /*weighted_c2, weighted_c3 , weighted_c4, weighted_c5, weighted_c6, weighted_w2, weighted_w3 , weighted_w4, weighted_w5,weighted_w6,
                                          nodes_c2, nodes_c3 , nodes_c4, nodes_c5, nodes_c6, nodes_w2, nodes_w3 , nodes_w4, nodes_w5,nodes_w6 //diffs_n_b
 
@@ -743,12 +743,14 @@ function Patients() {
                     [
                         // bigrams_c2, bigrams_c3 , bigrams_c4,
                         weighted_w6,
+                        weighted_w30,
                         // bigrams_w2, bigrams_w3 , bigrams_w4,
-                        bigrams_w6,
+                        // bigrams_w6,
                         // nodes_c2, nodes_c3 , nodes_c4,
                         // nodes_c6,
                         // nodes_w2, nodes_w3 , nodes_w4,
                         nodes_w6,// diffs_n_b
+                        nodes_w30,// diffs_n_b
                     ],
                     "groupId",
                     [
@@ -756,22 +758,28 @@ function Patients() {
                         // "Weighted, Comp. k=6",
                         // "Bigrams, Weight. k=2", "Bigrams, Weight. k=3","Bigrams, Weight. k=4",
                         "Weighted, Weight. k=6",
-                        "Bigrams, Weight. k=6",
+                        "Weighted, Weight. k=30",
+                        // "Bigrams, Weight. k=6",
                         // "Labels, Comp. k=2", "Labels, Comp. k=3","Labels, Comp. k=4",
                         // "Labels, Comp. k=6",
                         // "Labels, Weight. k=2", "Labels, Weight. k=3","Labels, Weight. k=4",
-                        "Labels, Weight. k=6",// "Diffs, Labels & Bigrams"
+                        "Labels, Weight. k=6",
+                        "Labels, Weight. k=30",
+                        // "Diffs, Labels & Bigrams"
                     ] ),
                     patient_dendogramIds = parse_clusters(patient.id,
                     [
                         // bigrams_c2, bigrams_c3 , bigrams_c4,
                         weighted_w6,
+                        weighted_w30,
                         // bigrams_w2, bigrams_w3 , bigrams_w4,
-                        bigrams_w6,
+                        // bigrams_w6,
                         // nodes_c2, nodes_c3 , nodes_c4,
                         // nodes_c6,
                         // nodes_w2, nodes_w3 , nodes_w4,
-                        nodes_w6,// diffs_n_b
+                        nodes_w6,
+                        nodes_w30,
+                        // diffs_n_b
                     ],
                     "dendogramId",
                     [
@@ -779,11 +787,14 @@ function Patients() {
                         // "Weighted, Comp. k=6",
                         // "Bigrams, Weight. k=2", "Bigrams, Weight. k=3","Bigrams, Weight. k=4",
                         "Weighted, Weight. k=6",
-                        "Bigrams, Weight. k=6",
+                        "Weighted, Weight. k=30",
+                        // "Bigrams, Weight. k=6",
                         // "Labels, Comp. k=2", "Labels, Comp. k=3","Labels, Comp. k=4",
                         // "Labels, Comp. k=6",
                         // "Labels, Weight. k=2", "Labels, Weight. k=3","Labels, Weight. k=4",
-                        "Labels, Weight. k=6",// "Diffs, Labels & Bigrams"
+                        "Labels, Weight. k=6",
+                        "Labels, Weight. k=30",
+                        // "Diffs, Labels & Bigrams"
                     ] ),
                     between = [],
                     nodes = extract_nodes(patient, between);
