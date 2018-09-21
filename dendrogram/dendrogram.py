f3576430029c78b5ad916642676c9ec66787e360
timothy <onorinbejasus@gmail.com>
2018-09-21 01:14:18 -0500

    -- The dendrogram works

--- dendrogram/dendrogram.py
+++ dendrogram/dendrogram.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+from scipy.cluster import hierarchy
 from scipy.cluster.hierarchy import dendrogram, linkage
 import numpy as np
 import sys
@@ -13,3 +14,20 @@ similarity_matrix = pd.read_csv(similarity_matrix_file, index_col=False, usecols
 
 Z = linkage(similarity_matrix, 'weighted')
 
+plt.figure(figsize=(25, 10))
+plt.title('Hierarchical Clustering Dendrogram')
+plt.xlabel('sample index')
+plt.ylabel('distance')
+
+hier = hierarchy.leaves_list(Z)
+
+tree = dendrogram(
+    Z,
+    truncate_mode='lastp',
+    p=25,
+    leaf_rotation=90.,  # rotates the x axis labels
+    leaf_font_size=8,  # font size for the x axis labels
+    color_threshold=5.4
+)
+
+plt.show()
