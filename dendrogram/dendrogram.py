9a391b2af2e8dd0d63868547e036366e73d2fa46
onorinbejasus <tluciani21@gmail.com>
2018-09-24 16:20:55 -0500

    -- Finished condensing the dendrogram. Color and patterns are next

--- dendrogram/dendrogram.py
+++ dendrogram/dendrogram.py
@@ -7,18 +7,50 @@ import numpy as np
 import sys
 import pandas as pd
 
+def get_children(root):
+    global names
+    fringe = [dict( node=root )]
+    current = fringe.pop()
+    children = []
+
+    while current and current['node']:
+        node = current['node']
+
+        if node.left:
+            fringe.append(dict( node=node.left ))
+
+        if node.right:
+            fringe.append(dict( node=node.right ))
+
+        if not node.left and not node.right:
+            children.append(names[node.id].split(" ")[1])
+
+        if len(fringe):
+            current = fringe.pop()
+        else:
+            current = None
+
+    return children
+
+
 #https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
-
-
 # Create a nested dictionary from the ClusterNode's returned by SciPy
 def add_node(node, parent):
+    global leaves
     # First create the new node and append it to its parent's children
-    newNode = dict( node_id=node.id, children=[] )
+    newNode = dict( node_id=node.id, children=[], count=node.count, dist=node.dist, cluster=[] )
     parent["children"].append( newNode )
 
-    # Recursively add the current node's children
-    if node.left: add_node( node.left, newNode )
-    if node.right: add_node( node.right, newNode )
+    # Recursively add the current node's children if the node is not a leaf
+    if node.dist > 0:
+        if node.left: add_node( node.left, newNode )
+        if node.right: add_node( node.right, newNode )
+
+    else:
+        left_children = get_children(node.left)
+        right_children = get_children(node.right)
+
+        newNode['cluster'] = sorted(left_children + right_children)
 
 
 # Label each node with the names of each leaf in its subtree
@@ -43,6 +75,8 @@ def label_tree( n ):
 similarity_matrix_file = sys.argv[1]
 similarity_matrix = pd.read_csv(similarity_matrix_file, index_col=False, usecols=range(1,584))
 
+names = similarity_matrix.columns
+
 labels = np.asarray(range(1, 584))
 id2name = dict(zip(range(len(labels)), labels))
 
@@ -62,11 +96,13 @@ tree = dendrogram(
     color_threshold=5.4,
     labels=labels, orientation='top'
 )
-plt.show()
 
 T = hierarchy.to_tree( Z , rd=False )
+leaves = hierarchy.leaves_list(Z)
+
 d3Dendro = dict(children=[], name="Root1")
 add_node( T, d3Dendro )
-label_tree( d3Dendro["children"][0] )
+#label_tree( d3Dendro["children"][0] )
 json.dump(d3Dendro, open("d3-dendrogram.json", "w"), sort_keys=True, indent=4)
 
+plt.show()
