16d750e7fe4dce24dc84bfcc171edab54e86c272
timothy <onorinbejasus@gmail.com>
2018-09-27 00:58:43 -0500

    -- Calculated the consensus patterns between patient clusters

--- dendrogram/js/utilities.js
+++ dendrogram/js/utilities.js
@@ -6,6 +6,8 @@ const Utilities = (function() {
   function Utilities() {
 
     let self = this;
+    self.patients = null;
+    self.consensus = {};
 
     this.extractNodes = function(patient, between){
       return _.chain(patient.nodes)
@@ -15,7 +17,6 @@ const Utilities = (function() {
           between.push(value);
         }
         else {
-          result.push(value);
           if(value.length === 2 && value[1] === "2"){
             result.push(value[0] + value[1] + 'A');
             result.push(value[0] + value[1] + 'B');
@@ -26,17 +27,13 @@ const Utilities = (function() {
         }
         return result;
       }, [])
-      .partition(function (p) {
-        return p[0] === 'L';
-      }).value();
+      .partition(function (p) { return p[0] === 'L';}).value();
 
     }
 
   }
 
   Utilities.prototype.iterativeInOrder = function(node, cb) {
-    let self = this;
-
     let stack = []
       , currentNode = node;
 
@@ -55,7 +52,6 @@ const Utilities = (function() {
   };
 
   Utilities.prototype.parsePatientData = function(patients) {
-
     let self = this, parsed_patients = {};
 
     patients.forEach(function(patient){
@@ -78,8 +74,69 @@ const Utilities = (function() {
     self.patients = parsed_patients;
   };
 
-  Utilities.prototype.getGroupConsensus = function(group) {
+  Utilities.prototype.getGroupConsensus = function(clusters) {
+    let self = this;
 
+    self.iterativeInOrder(clusters[0], function(node){
+      /* Make sure the node has the clustered patients */
+      if(!(node.cluster && node.cluster.length > 0)) return;
+
+      let involvement_occurrences = { "a":{}, "b":{} };
+      /* Iterate over the ids and get the patients' nodes */
+      node.cluster.forEach(function(p_id) {
+
+        let patient_nodes = _.flatten(self.patients[p_id].nodes);
+
+        let levels = [];
+        patient_nodes.forEach(function (involvement) {
+          levels.push(involvement.substr(1));
+        });
+
+        let bilateral = _.filter(levels, (val, i, iteratee) => _.includes(iteratee, val, i + 1))
+          , unilateral = _.difference(levels, bilateral);
+
+        /* Add the bilateral nodes*/
+        bilateral.forEach(function(involvement){
+          /* Check if the first side has recorded this involvement */
+          if(_.has(involvement_occurrences["a"], involvement)) {
+            involvement_occurrences["a"][involvement] += 1;
+          }
+          else {
+            involvement_occurrences["a"][involvement] = 1;
+          }
+          /* Check if the second side has recorded this involvement */
+          if(_.has(involvement_occurrences["b"], involvement)) {
+            involvement_occurrences["b"][involvement] += 1;
+          }
+          else {
+            involvement_occurrences["b"][involvement] = 1;
+          }
+
+        });
+
+        unilateral.forEach(function(involvement) {
+          /* Check if the first side has recorded this involvement */
+          if (_.has(involvement_occurrences["a"], involvement)) {
+            involvement_occurrences["a"][involvement] += 1;
+          }
+          else {
+            involvement_occurrences["a"][involvement] = 1;
+          }
+        });
+      });
+
+      /* Get the percentage involvement */
+      _.forIn(involvement_occurrences["a"], (value, key)=>{involvement_occurrences["a"][key] = value / node.count;});
+      _.forIn(involvement_occurrences["b"], (value, key) =>{involvement_occurrences["a"][key] = value / node.count;});
+
+      /* Save the percentages */
+      self.consensus[node.node_id] = involvement_occurrences;
+    });
+
+  };
+
+  Utilities.prototype.getConsensus = function(id) {
+    return (id) ? this.consensus[id] : this.consensus
   };
 
   return Utilities;
