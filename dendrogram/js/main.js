b75c68ced0c1b6c4a99ffc3f7ecc715ff6022ab0
onorinbejasus <tluciani21@gmail.com>
2018-09-28 16:27:14 -0500

    -- Added the spatial and non-spatial data to the code

--- dendrogram/js/main.js
+++ dendrogram/js/main.js
@@ -10,7 +10,7 @@ var App = App || {};
 
   function init() {
     queue()
-      .defer(d3.json, "d3-dendrogram.json")
+      .defer(d3.json, "non-spatial_hierarchy.json")
       .defer(d3.json, "../data/1.3.1/json/tanimoto_weighted_UPPER.json")
       .await(function(err, dendrogram, patients){
 
